26901ab61683e3f040edfbbb796c3853e08f09e2
2022phyro <phyrokelstein2@gmail.com>
2023-04-06 23:49:08 +0100

    Added 1-pack_web_static.py to 2-do_deploy_web_static.py to properly base my answer on that file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
 """This file uses fabric to generate a .tgz archive
 from the contents of AirBnB_Clone using do_pack"""
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local
+import datetime
 import os
 
 
+
 env.hosts = ['3.84.238.62', '100.25.17.233']
+def do_pack():
+    """ This uses fabric to create a tar file"""
+    date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "versions/web_static_{}.tgz".format(date)
+    command = "tar -cvzf {} web_static".format(name)
+    if not os.path.isdir("versions"):
+        res = local('mkdir versions')
+    res = local(command)
+    if res.failed:
+        return None
+    return name
+
 def do_deploy(archive_path):
     """ This uses fabric to deploy a tar file to the server"""
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         return False
     try:
         name = archive_path.split('/')[-1]
@@ -25,5 +39,9 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(file_))
         return True
-    except Exception as e:
+    except Exception:
         return False
+
+
+
+
