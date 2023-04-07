25509e0317fe36a00f911b0a97c9647ad9013610
2022phyro <phyrokelstein2@gmail.com>
2023-04-07 23:07:17 +0100

    No 0 and $, I reconfigured my server to have sites_enabled as symlinks not actual  files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,7 +7,7 @@ import os
 from fabric.decorators import runs_once
 
 
-env.hosts = ['3.84.238.62', '100.25.17.233']
+env.hosts = ['100.25.17.233', '3.84.238.62']
 @runs_once
 def do_pack():
     """ This uses fabric to create a tar file"""
@@ -51,5 +51,28 @@ def deploy():
     val = do_deploy(name)
     return val
 
-def do_clean():
+@runs_once
+def clean_versions(list_):
+     [os.remove("versions/{}".format(fil)) for fil in list_]
+     return list_
 
+def do_clean(number=0):
+    """This leaves only the most recent versions"""
+    number = int(number)
+    if not os.path.isdir('versions'):
+        return
+    x = os.listdir('versions')
+    y = [int(m.strip("web_static_.tgz")) for m in x]
+    if number == 0:
+        y.remove(max(y))
+    else:
+        for num in range(number):
+            if y == []:
+                return
+            y.remove(max(y))
+    to_delete = [val for val in x if int(val.strip("web_static_.tgz")) in y]
+    final = clean_versions(to_delete)
+    #va = run('ls -1 /data/web_static/releases/').split("\n")
+    for val in final:
+        dir_name = val.strip(".tgz")
+        run("sudo rm -r /data/web_static/releases/{}".format(dir_name))
