704912f5788074fba9c881a3a1cbdb6c18e4118d
2022phyro <phyrokelstein2@gmail.com>
2023-04-21 19:33:59 +0100

    improve sql alchemy

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,3 +76,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = Session()
+
+    def close(self):
+        """Closes the session"""
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,3 +66,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """This closes a sessions"""
+        self.reload()
